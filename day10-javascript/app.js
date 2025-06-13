bac5776117d2b6f7fa9bf8bdebad656f049b5b0c
Akash13103 <akashmurala13103@gmail.com>
2025-06-13 15:08:41 +0530

    cmmit

--- day10-javascript/app.js
+++ day10-javascript/app.js
@@ -10,3 +10,16 @@ let phoneNumber; // undefined
 let middleName = null; // intentionally empty
 console.log("Phone:", phoneNumber);
 console.log("Middle name:", middleName);
+const name = "Rajesh";
+function manMade(){
+    let name1 = "Kartik";
+    name1="Kap";
+    if(true){
+        console.log(name1);
+    }
+    console.log(name1);
+    return name1;
+}
+console.log(manMade.name1);//returns undefined due to block scope variable 'name1' using let;
+console.log(manMade());
+manMade();
