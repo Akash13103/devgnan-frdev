c5446b14c9f5931378787324de2a30ba0da05bf1
Akash13103 <akashmurala13103@gmail.com>
2025-06-17 18:01:05 +0530

    commit

---
+++ day10-javascript/day 14 practice.js
@@ -0,0 +1,23 @@
+const arrData = [{
+    name: "Akash",
+    UserID: "12BVE"
+}, {
+    name: "Rohan",
+    UserID: "13BVE"
+}, {
+    name: "Maxx",
+    UserID: "14BVE"
+}, {
+    name: "Mohan",
+    UserID: "15BVE"
+}, {
+    name: "Kalyan",
+    UserID: "16BVE"
+}, {
+    name: "Ryuuji",
+    UserID: "17BVE"
+}]
+const me=arrData.map(function callBack(arrData,index,n){
+    return arrData.name+" "+index
+})
+console.log(me)
