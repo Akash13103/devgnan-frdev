3c7792fa4562735ae9c334558f380c147339522b
Akash13103 <akashmurala13103@gmail.com>
2025-06-24 16:43:40 +0530

    login jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from "./Components/Login.jsx"
-import './Components/Login/Login.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Login from "./Components/Login.jsx";
 function App() {
   const [count, setCount] = useState(0)
 
