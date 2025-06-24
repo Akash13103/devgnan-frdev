69c4f0f4f94b2d10b5337fa13965000a0a1e52f7
Akash13103 <akashmurala13103@gmail.com>
2025-06-24 19:02:42 +0530

    commit

--- my-react-app/src/Components/NavBar/NavBar.jsx
+++ my-react-app/src/Components/NavBar/NavBar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 function NavBar() {
   return (
     <>
-      <div className="navbar">
+        <div className="navbar">
         <div>Dashboard</div>
         <div>Orders (`/orders`)</div>
         <div>Holdings</div>
         <div>Positions</div>
         <div>Funds </div>
       </div>
+
     </>
   )
 }
