47025c269b1b8e16842b12ae397a188b4806ff9c
Akash13103 <akashmurala13103@gmail.com>
2025-06-25 12:55:29 +0530

    commit

--- my-react-app/src/Components/Login/Login.jsx
+++ my-react-app/src/Components/Login/Login.jsx
@@ -2,36 +2,43 @@ import React from "react"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Login() {
-    const [username,updateUserName]=useState("Akash")
-    const [password,setPassword]=useState("")
-    const navigate=useNavigate("");
-    function handleUserName(event){
-        console.log("UserName:",event.target.value)
+    const [username, updateUserName] = useState("Akash")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate("");
+    function handleUserName(event) {
+        console.log("UserName:", event.target.value)
         updateUserName(event.target.value)
     }
-    function handlePassword(event){
-        console.log("Password:",event.target.value)
+    function handlePassword(event) {
+        console.log("Password:", event.target.value)
         setPassword(event.target.value)
     }
-    function handleLoginButton(){
-        console.log(username,password)
-        if(username === "Akash" && password === "123Kap"){
+    function handleLoginButton() {
+        console.log(username, password)
+        axios.post(
+            https://api.escuelajs.co/api/v1/auth/login
+            {
+                "email": "Akash",
+                "password": "123Kap"
+            }
+        ).then()
+        if (username === "Akash" && password === "123Kap") {
             navigate('/dashboard')
         }
-        else{
+        else {
             alert("User is invalid")
         }
     }
     return (
-        <> 
+        <>
             <div className="login-container">
                 <div className="login-card">
                     <h5>Login to Kite</h5>
-                        <div className="input-field">Phone or UserID</div>
-                        <input type="text"  onChange={handleUserName} placeholder="Phone or UserID" required />
-                        <div className="input-field">Password</div>
-                        <input type="password" onChange={handlePassword} placeholder="Password" required />
-                        <button className="login-button" onClick={handleLoginButton}>Login</button>
+                    <div className="input-field">Phone or UserID</div>
+                    <input type="text" onChange={handleUserName} placeholder="Phone or UserID" required />
+                    <div className="input-field">Password</div>
+                    <input type="password" onChange={handlePassword} placeholder="Password" required />
+                    <button className="login-button" onClick={handleLoginButton}>Login</button>
                     <div className="forgot-link"><a href="*">Forgot User ID or password?</a></div>
                 </div>
             </div>
