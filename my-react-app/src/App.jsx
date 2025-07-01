7c9f001bde0edca546c491711a435f6d5f9f255c
Akash13103 <akashmurala13103@gmail.com>
2025-07-01 18:53:28 +0530

    commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,25 +3,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./Components/Login/Login.jsx";
-import { BrowserRouter, Routes, Route, useLocation,Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Link,
+} from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar.jsx";
 import "./Components/Login/Login.css";
-import './Components/DashBoard/DashBoard.css'
+import "./Components/DashBoard/DashBoard.css";
 import DashBoard from "./Components/DashBoard/DashBoard.jsx";
 import Orders from "./Components/Orders/Orders.jsx";
 import Holdings from "./Components/Holdings/Holdings.jsx";
 import Positions from "./Components/Positions/Positions.jsx";
 import Funds from "./Components/Funds/Funds.jsx";
 import Bids from "./Components/Bids/Bids.jsx";
+
 function App() {
   const [count, setCount] = useState(0);
-  const location=useLocation();
-  console.log("location:",location.pathname);
+  const location = useLocation();
+  console.log("location:", location.pathname);
 
   return (
     <>
-       {
-        location.pathname==="/"? null :  <div>
+      {location.pathname === "/" ? null : (
+        <div>
           <ul className="navbar-container">
             <li>
               <Link to="/dashboard">DashBoard</Link>
@@ -48,17 +55,17 @@ function App() {
             </li>
           </ul>
         </div>
-       }
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<NavBar />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/bids" element={<Bids />} />
-        </Routes>
+      )}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<NavBar />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/holdings" element={<Holdings />} />
+        <Route path="/positions" element={<Positions />} />
+        <Route path="/funds" element={<Funds />} />
+        <Route path="/bids" element={<Bids />} />
+      </Routes>
     </>
   );
 }
--- my-react-app/src/Components/Holdings/Holdings.jsx
+++ my-react-app/src/Components/Holdings/Holdings.jsx
@@ -1,32 +1,119 @@
-import React from 'react';
-import BasicTable from '../Table/BasicTable';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "../Holdings/Holdings.css";
+import Grid from "@mui/material/Grid";
+
+const rows = [
+  {
+    Instrument: "ADANIET",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANI",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANIE",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANIETS",
+    Quantity: 28,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANIETU",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANIETM",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+  {
+    Instrument: "ADANIETN",
+    Quantity: 20,
+    Avg_Cost: 2197.84,
+    Invested: 43956.6,
+  },
+];
 
 export default function Holdings() {
-    const holdingData=[{
-        "Instrument":"ADANIREET",
-        "avg_cost":2319.75,
-        "day_chg":0,
-        "net_chg":320
-    },{
-        "Instrument":"ADANIREET",
-        "avg_cost":2319.75,
-        "day_chg":0,
-        "net_chg":320
-    },{
-        "Instrument":"ADANIREET",
-        "avg_cost":2319.75,
-        "day_chg":0,
-        "net_chg":320
-    }]
-    holdingData.map(function(index,newArray){
-        return{
-            
-        }
-    })
   return (
-    // Component content
-    <div>
-        <BasicTable />
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Grid size={3}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Instrument</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.Instrument}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.Instrument}
+                    </TableCell>
+                    <TableCell align="right">{row.Quantity}</TableCell>
+                    <TableCell align="right">{row.Avg_Cost}</TableCell>
+                    <TableCell align="right">{row.Invested}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid size={9}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Instrument</TableCell>
+                  <TableCell align="right">Quantity</TableCell>
+                  <TableCell align="right">Avg_Cost</TableCell>
+                  <TableCell align="right">Invested</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.Instrument}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.Instrument}
+                    </TableCell>
+                    <TableCell align="right">{row.Quantity}</TableCell>
+                    <TableCell align="right">{row.Avg_Cost}</TableCell>
+                    <TableCell align="right">{row.Invested}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </>
   );
 }
