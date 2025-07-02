05e0421b3ad4df0170d50c4c740816530bca233c
Akash13103 <akashmurala13103@gmail.com>
2025-07-02 18:57:34 +0530

    commit

--- my-react-app/src/Components/DashBoard/DashBoard.jsx
+++ my-react-app/src/Components/DashBoard/DashBoard.jsx
@@ -10,88 +10,12 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import '../Holdings/Holdings.css'
-class DashBoard extends Component {
-  render() {
-    const rows = [
-  {
-    Instrument: "ADANIET",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANI",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANIE",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANIETS",
-    Quantity: 28,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANIETU",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANIETM",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-  {
-    Instrument: "ADANIETN",
-    Quantity: 20,
-    Avg_Cost: 2197.84,
-    Invested: 43956.6,
-  },
-];
-    return (
-      <>
-        <Grid container spacing={1}>
-          <Grid size={3}>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Instrument</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow
-                      key={row.Instrument}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.Instrument}
-                      </TableCell>
-                      <TableCell align="right">{row.Quantity}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <Grid size={8}>
-              Equity
-              Commodity
-            </Grid>
-          </Grid>
-        </Grid>
-      </>
-    );
-  }
-}
+import Holdings from '../Holdings/Holdings.jsx'
 
-export default DashBoard;
+export default function DashBoard() {
+  return (
+    <>
+      <Holdings/>
+    </>
+  );
+}
