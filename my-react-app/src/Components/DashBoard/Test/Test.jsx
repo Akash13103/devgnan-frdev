7bc0f56ec9bbc3f24e2fae8c88676e5487cf8814
Akash13103 <akashmurala13103@gmail.com>
2025-08-26 10:02:29 +0530

    commit

--- my-react-app/src/Components/DashBoard/Test/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return <div></div>;
-};
-
-export default Test;
-//Redux Toolkit is a global state management it is also called as  predictable state container .it is to overcome the problem of context api
-// redux slices==>loginform slice==>store.js==>main.jsx provider adding==> useSelector()
